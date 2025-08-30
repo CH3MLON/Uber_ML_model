756299cc4ca27accb5208f0699b49528f8e0a956
CH3MLON <rohithkarthik275@gmail.com>
2025-08-30 16:55:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, render_template
+import math
+import pickle
+
+app=Flask(__name__)
+
+model2 = pickle.load(open("model.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[int(i) for i in request.form.values()]
+
+    final_feature=np.array(int_features).reshape(1,-1)#[[1,2,3,4]]
+
+    prediction=model2.predict(final_feature)
+    output=round(prediction[0],2)
+    return render_template('index.html',predict_text='Number of weekly riders is {}'.format(math.floor(output)))
+if __name__=="__main__":
+    app.run(debug=True)
